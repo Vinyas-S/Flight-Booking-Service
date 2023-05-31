88e3066f27ebf5d890e03057d3867f690ed3cabb
Vinyas <svinyas2000@gmail.com>
2023-06-01 02:08:59 +0530

    started booking impl

---
+++ controllers/booking-controller.js
@@ -0,0 +1,26 @@
+const {BookingService}= require('../services')
+
+const {StatusCodes}= require("http-status-codes")
+const {SuccessResponse , ErrorResponse} = require('../utils/common')
+
+async function createBooking(req,res){
+    try {
+        const airport =await BookingService.createBooking({
+            flightId: req.body.flightId,
+            userId:req.body.userId,
+            noOfSeats: req.body.noOfSeats
+        })
+        SuccessResponse.data = airport
+        return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse)
+    } catch (error) {
+        ErrorResponse.error = error
+        return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .json(ErrorResponse) 
+    }
+}
+module.exports = {
+    createBooking
+}
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const router =express.Router();
-
+const bookingRoutes = require('./booking')
 const {InfoController} = require('../../controllers/index')
 router.get('/info', InfoController.info)
-
+router.use('/bookings',bookingRoutes)
 module.exports = router
